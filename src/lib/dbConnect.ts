118c43eae6ef817900e2491dbb70c4255b00403e
Harsh Chandwani <harshchandwani50028@gmail.com>
2024-09-01 14:23:44 +0530

    Added DbConnection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection: ConnectionObject = {}
+
+async function dbConnect(): Promise<void> {
+    //in typescipt, void means, any type of data, not like C++
+    if (connection.isConnected) {
+        console.log("Already Connected")
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("Db Connected Successfully");
+    }
+    catch (error) {
+        console.log("Database Connection failed", error);
+        process.exit(1);
+    }
+}
+export default dbConnect;
