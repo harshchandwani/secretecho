f9f8c54ea999d8659eec56317b5f47c95577353e
Harsh Chandwani <harshchandwani50028@gmail.com>
2024-09-01 11:50:55 +0530

    User Model Implemented

--- src/model/User.ts
+++ src/model/User.ts
@@ -12,10 +12,10 @@ export interface User extends Document {
     email: string;
     password: string;
     verifyCode: string;
-    verifyCodeExpiry: string;
+    verifyCodeExpiry: Date;
     isVerified: boolean;
     isAcceptingMessge: boolean;
-    message: Message[];
+    messages: Message[];
 }
 const MessageSchema: Schema<Message> = new Schema({
     content: {
@@ -45,22 +45,27 @@ const UserSchema: Schema<User> = new Schema({
     password: {
         type: String,
         required: [true, "Password is required"],
-        match: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm, "Please Enter a Valid Password"]
+        match: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm, "Please Enter a Valid Password"] //We will check on this
     },
     verifyCode: {
         type: String,
         required: [true, "Verify Code is required"]
     },
     verifyCodeExpiry: {
-        type: String,
-        required: [true, "Verify Code Expiry is required"]
+        type: Date
     },
     isAcceptingMessge: {
         type: Boolean,
-        required: [true, "Accepting Message is required"]
+        default: true
     },
-    message: {
-        type: Message[],
-        required: [true, "Verify Code is required"]
-    }
+    messages: [
+        MessageSchema
+    ]
 })
+// Check if the User model is already defined in Mongoose models. 
+// If it is, use the existing model. This prevents redefining the model, 
+// which can cause errors if the code is run multiple times (e.g., in serverless functions).
+// If the User model is not defined, define a new model using the UserSchema.
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
